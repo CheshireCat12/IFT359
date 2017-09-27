c84dd1f76d8c39b20be5ddcdbaa469110162997c
dany <dany.chea@protonmail.ch>
2017-09-27 15:12:38 -0400

    n°2 completed

--- TP02/no2.rkt
+++ TP02/no2.rkt
@@ -1,12 +1,48 @@
 #lang racket
 
-; nom1
-; nom2
+; Chea Dany 17 133 040
+; Gillioz Anthony 17 133 031
 
 (provide intersection3)
 (require (file "codedesdiapositives.rkt"))
 
 (define intersection3  
   (λ (ls1 ls2 ls3)
-    "votre code"))
+    (cond
+      [(null? ls1)'()]
+      [(null? ls2)'()]
+      [(null? ls3)'()]
+     
+      [else
+       (cond
+         [(gt (car ls1) (car ls2)) (intersection3  ls1 (cdr ls2) ls3)]
+         [else
+          (cond
+            [(gt (car ls2) (car ls1)) (intersection3 (cdr ls1) ls2 ls3)]
+            [else
+             (cond
+               [(gt (car ls1) (car ls3))(intersection3 ls1 ls2 (cdr ls3))]
+               [else
+                (cond
+                  [(gt (car ls3) (car ls1))(intersection3 (cdr ls1) (cdr ls2) ls3)]
+                  [else
+                   (cond
+                     [(equal? (car ls1) (car ls3))(cons (car ls1) (intersection3(cdr ls1)(cdr ls2)(cdr ls3)))]
+                     )
+                   ]
+                  )
+                ]
+               )
+             ]
+            )
+          ]
+         )        
+       ]
+      )
+      
 
+
+    ))
+(intersection3 '(1 5 8 12) '(1 2 5 8) '(1 8)) ;renvoie (1 8)
+(intersection3 '(1 5 8 12) '(1 2 5 9) '(1 8)) ; renvoie (1)
+(intersection3 '(2 5 8 12) '(1 2 5 9) '() ) ; renvoie ()
